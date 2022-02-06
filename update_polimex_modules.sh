071e7b469396bb573e5685dc01b67267327417e1
Lubo <lgeorgiev@securitybulgaria.com>
2022-02-06 13:27:55 +0200

    Fix

--- update_polimex_modules.sh
+++ update_polimex_modules.sh
@@ -5,6 +5,6 @@ sudo -H -u odoo15 bash -c 'cd /opt/odoo15/custom-addons/polimex-rfid/ && git pul
 echo 'Updating Polimex modules in database'
 sudo -H -u odoo15 bash -c '/opt/odoo15/venv/bin/python3 /opt/odoo15/odoo/odoo-bin -d 15_polimexodoo --addons-path /opt/odoo15/odoo/addons,/opt/odoo15/addons -u hr_rfid --stop-after-init'
 echo 'Removing current Odoo sessions (need browser refresh'
-sudo -H -u odoo15 bash -c '/opt/odoo15/.local/share/Odoo/sessions/rm *.sess'
+sudo -H -u odoo15 bash -c 'cd /opt/odoo15/.local/share/Odoo/sessions/ && rm *.sess'
 sudo systemctl start odoo15
 tail -f /var/log/odoo15.log

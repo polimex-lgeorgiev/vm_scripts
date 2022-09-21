e777cdf1bfc97e89d656ccdd2fd280a149775fe5
Lyubomir Georgiev <lgeorgiev@securitybulgaria.com>
2022-09-21 13:44:08 +0300

    add some params

--- update_polimex_modules.sh
+++ update_polimex_modules.sh
@@ -7,10 +7,10 @@ ODOO_MODULE=hr_rfid
 echo 'Stop Odoo Service before update'
 sudo systemctl stop ${ODOO_SERVICE}
 echo 'Downloading Polimex modules'
-sudo -H -u ${ODOO_USER} bash -c 'cd /opt/odoo15/custom-addons/polimex-rfid/ && git pull'
+sudo -H -u ${ODOO_USER} bash -c "cd /opt/${ODOO_USER}/custom-addons/polimex-rfid/ && git pull"
 echo 'Updating Polimex modules in database'
-sudo -H -u ${ODOO_USER} bash -c "/opt/odoo15/venv/bin/python3 /opt/odoo15/odoo/odoo-bin -d ${ODOO_DATABASE} --addons-path /opt/odoo15/odoo/addons,/opt/odoo15/addons -u ${ODOO_MODULE} --stop-after-init"
+sudo -H -u ${ODOO_USER} bash -c "/opt/${ODOO_USER}/venv/bin/python3 /opt/${ODOO_USER}/odoo/odoo-bin -d ${ODOO_DATABASE} --addons-path /opt/${ODOO_USER}/odoo/addons,/opt/${ODOO_USER}/addons -u ${ODOO_MODULE} --stop-after-init"
 echo 'Removing current Odoo sessions (need browser refresh)'
-sudo -H -u ${ODOO_USER} bash -c 'cd /opt/odoo15/.local/share/Odoo/sessions/ && rm *.sess'
+sudo -H -u ${ODOO_USER} bash -c "cd /opt/${ODOO_USER}/.local/share/Odoo/sessions/ && rm *.sess"
 sudo systemctl start ${ODOO_SERVICE}
-tail -f /var/log/${ODOO_DATABASE}.log
+tail -f /var/log/${ODOO_USER}.log

dc5f8c965a7f6af18ee46d39e08af76894c0cf28
Lubo <lgeorgiev@securitybulgaria.com>
2022-12-13 16:27:01 +0200

    make static address

--- make_dynamic_ip.sh
+++ make_dynamic_ip.sh
@@ -1,3 +1,5 @@
 echo "This script will make IP address dynamic of the virtual machine."
+ip a
 sudo cp ~/vm_scripts/dhcp_template.yaml /etc/netplan/00-installer-config.yaml
 sudo netplan apply
+ip a

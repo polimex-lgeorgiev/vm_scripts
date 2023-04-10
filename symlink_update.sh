5a91711b67db183e09a61f4eaa84b3c1c81ae9e9
Lubo <lgeorgiev@securitybulgaria.com>
2023-04-10 11:43:55 +0300

    symlink_update.sh added

--- symlink_update.sh
+++ symlink_update.sh
@@ -60,12 +60,12 @@ fi
 # Find all subfolders with a __manifest__.py file and create symlinks
 find "$src_folder" -type f -name "__manifest__.py" -exec dirname {} \; | while read -r folder; do
     folder_name=\$(basename "\$folder")
-    symlink_target="\$dest_folder/\$(basename "\$folder")"
+    symlink_target="\$dest_folder/\$folder_name"
 
     if [ -L "\$symlink_target" ]; then
         echo "Skipped symlink creation: \$symlink_target already exists"
     else
-        ln -s "\$folder" "\$symlink_target"
+        ln -s "\$(realpath "\$folder")" "\$symlink_target"
         echo "Created symlink: \$symlink_target -> \$folder"
     fi
 done

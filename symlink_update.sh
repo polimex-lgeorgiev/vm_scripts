e4039d90309b142122b834a9cff4b82ba39cb9ee
Lubo <lgeorgiev@securitybulgaria.com>
2023-04-10 11:18:27 +0300

    symlink_update.sh added

--- symlink_update.sh
+++ symlink_update.sh
@@ -5,7 +5,8 @@ usage() {
     echo "Usage: $0 [-u user] [source_folder] [destination_folder]"
     echo
     echo "Options:"
-    echo "  -u  Run the script as a specific user (default: current user)"
+    echo "  -u  Run the script as a specific user (default: odoo15)"
+    echo "  -h, --help  Show help information"
     echo
     echo "This script creates symlinks for all subfolders containing a __manifest__.py file."
     echo "Default source folder: /opt/odoo15/custom-addons"
@@ -14,11 +15,25 @@ usage() {
 }
 
 # Parse command-line options
-user="$USER"
-while getopts ":u:" opt; do
+user="odoo15"
+while getopts ":u:-:" opt; do
     case $opt in
         u)
             user="$OPTARG"
+            ;;
+        -)
+            case $OPTARG in
+                help)
+                    usage
+                    ;;
+                *)
+                    echo "Unknown option: --$OPTARG"
+                    usage
+                    ;;
+            esac
+            ;;
+        h)
+            usage
             ;;
         *)
             usage
@@ -31,6 +46,11 @@ shift $((OPTIND-1))
 src_folder="${1:-/opt/odoo15/custom-addons}"
 dest_folder="${2:-/opt/odoo15/addons}"
 
+# Create a temporary script to run under the provided user
+temp_script=$(mktemp)
+cat << EOF > "$temp_script"
+#!/bin/bash
+
 # Check if folders exist
 if [ ! -d "$src_folder" ] || [ ! -d "$dest_folder" ]; then
     echo "Error: Source or destination folder does not exist."
@@ -39,13 +59,22 @@ fi
 
 # Find all subfolders with a __manifest__.py file and create symlinks
 find "$src_folder" -type f -name "__manifest__.py" -exec dirname {} \; | while read -r folder; do
-    folder_name=$(basename "$folder")
-    symlink_target="$dest_folder/$folder_name"
+    folder_name=\$(basename "\$folder")
+    symlink_target="\$dest_folder/\$folder_name"
 
-    if [ -L "$symlink_target" ]; then
-        echo "Skipped symlink creation: $symlink_target already exists"
+    if [ -L "\$symlink_target" ]; then
+        echo "Skipped symlink creation: \$symlink_target already exists"
     else
-        sudo -u "$user" ln -s "$folder" "$symlink_target"
-        echo "Created symlink: $symlink_target -> $folder"
+        ln -s "\$folder" "\$symlink_target"
+        echo "Created symlink: \$symlink_target -> \$folder"
     fi
 done
+EOF
+
+chmod +x "$temp_script"
+
+# Execute the temporary script as the provided user
+sudo -u "$user" "$temp_script"
+
+# Clean up the temporary script
+rm "$temp_script"
